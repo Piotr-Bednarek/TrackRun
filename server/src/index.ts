17cd1c2a1ac7cadd33b2ddef0f696004593191f0
Piotr-Bednarek <72524343+Piotr-Bednarek@users.noreply.github.com>
2023-03-03 00:37:57 +0100

    Even more firebase implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,16 @@ import * as Interface from "./models/Interfaces";
 
 import express from "express";
 
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-import { getAuth } from "firebase-admin/auth";
 
 import { getUidFromToken } from "./utils/authenticateUser";
 
@@ -23,6 +21,8 @@ const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+const db = admin.firestore();
 
 app.post("/api/login", (req, res) => {
   const data = req.body;
@@ -36,12 +36,24 @@ app.post("/api/test", (req, res) => {
   getUidFromToken(data.idToken)
     .then((uid) => {
       console.log(uid);
+      addUserToDatabase(uid);
       res.json({ success: true, uid: uid });
     })
     .catch((error) => {
       console.log(error);
     });
 });
+
+const addUserToDatabase = (uid: any) => {
+  const userRef = db.collection("users").doc(uid);
+  const runRef = db.collection("users").doc(uid).collection("runs").doc();
+
+  const run: Interface.Run = {
+    runId: runRef.id,
+  };
+
+  userRef.collection("runs").doc(run.runId).set(run);
+};
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
